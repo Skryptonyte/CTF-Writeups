5ffe2065ac7294eb8afac4fac182176854c7e258
Rayhan Faizel <rayhan.faizel@gmail.com>
2022-09-05 23:32:34 +0530

    Accidentally used old non working script

--- MapleCTF-2022/maplewave/maplewrite.c
+++ MapleCTF-2022/maplewave/maplewrite.c
@@ -3,7 +3,9 @@
 #include <pulse/simple.h>
 #include <pulse/error.h>
 
-int main()
+
+// Pacat sample code: https://freedesktop.org/software/pulseaudio/doxygen/pacat-simple_8c-example.html
+int main(int argc, char** argv)
 {
     char *buffer = malloc(1 << 18);
     FILE* f = fopen("flag0.maplewave","rb");
@@ -21,13 +23,25 @@ int main()
     }
 
     printf("%d bytes read!\n",i);
+ 
+    static const pa_sample_spec ss = {
+        .format = PA_SAMPLE_U8,
+        .rate = 44100 >> 2,
+        .channels = 1
+    };
     int error;
     pa_simple* s = pa_simple_new(NULL, argv[0], PA_STREAM_PLAYBACK, NULL, "playback", &ss, NULL, NULL, &error);
 
     if (!s)
+    {
         puts("Failed to create PulseAudio connection!");
+        return -1;
+    }
     
-    pa_simple_write(s,buffer+8,i-8,i++);
-
+    int increment = 8;
+    for (int j = 0; j < i-8; j+=increment)
+    {
+        pa_simple_write(s,buffer+8+j,increment,&error);
+    }
     pa_simple_drain(s, &error);
 }
